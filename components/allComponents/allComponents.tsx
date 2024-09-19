1d60f2d47ef37099adceabcea211023fab873cd1
Muhammad Kaif Nazeer <34789210-muhammadkaif057@users.noreply.replit.com>
2024-09-19 03:47:04 +0000

    feat: Implement Coming Soon component in all components and blocks pages

--- components/allComponents/allComponents.tsx
+++ components/allComponents/allComponents.tsx
@@ -32,6 +32,87 @@ const AllComponents: React.FC = () => {
             </p>
           </Link>
         ))}
+        <div className="antialiased h-full">
+          <div className="aspect-video py-10 flex-col h-72 md:h-full w-full relative overflow-hidden rounded-lg dark:bg-black dark:border-white/[0.1] border border-transparent bg-gray-50 bg-dot-slate-200 dark:bg-dot-white/[0.1] flex items-center justify-center">
+            <div className="text-4xl font-bold dark:text-neutral-200 text-neutral-500">
+              <span>Coming Soon</span>
+              <div className="opacity-20 absolute left-1/2 top-20 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_45%)] sm:-translate-x-1/2 lg:ml-12 xl:ml-0">
+                <svg
+                  viewBox="0 0 1026 1026"
+                  fill="none"
+                  aria-hidden="true"
+                  className="absolute inset-0 h-full w-full animate-spin-slow"
+                >
+                  <path
+                    d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
+                    stroke="#D4D4D4"
+                    strokeOpacity="0.7"
+                  ></path>
+                  <path
+                    d="M513 1025C230.23 1025 1 795.77 1 513"
+                    stroke="url(#x-s5_JMl_hi2LDEmLRP14-gradient-1)"
+                    strokeLinecap="round"
+                  ></path>
+                  <defs>
+                    <linearGradient
+                      id="x-s5_JMl_hi2LDEmLRP14-gradient-1"
+                      x1="1"
+                      y1="513"
+                      x2="1"
+                      y2="1025"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#06b6d4"></stop>
+                      <stop
+                        offset="1"
+                        stopColor="#06b6d4"
+                        stopOpacity="0"
+                      ></stop>
+                    </linearGradient>
+                  </defs>
+                </svg>
+                <svg
+                  viewBox="0 0 1026 1026"
+                  fill="none"
+                  aria-hidden="true"
+                  className="absolute top-[50%] left-[50%] inset-0 h-full w-full animate-spin-reverse-slower"
+                >
+                  <path
+                    d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
+                    stroke="#D4D4D4"
+                    strokeOpacity="0.7"
+                  ></path>
+                  <path
+                    d="M913 513c0 220.914-179.086 400-400 400"
+                    stroke="url(#x-s5_JMl_hi2LDEmLRP14-gradient-2)"
+                    strokeLinecap="round"
+                  ></path>
+                  <defs>
+                    <linearGradient
+                      id="x-s5_JMl_hi2LDEmLRP14-gradient-2"
+                      x1="913"
+                      y1="513"
+                      x2="913"
+                      y2="913"
+                      gradientUnits="userSpaceOnUse"
+                    >
+                      <stop stopColor="#06b6d4"></stop>
+                      <stop
+                        offset="1"
+                        stopColor="#06b6d4"
+                        stopOpacity="0"
+                      ></stop>
+                    </linearGradient>
+                  </defs>
+                </svg>
+              </div>
+            </div>
+            <p className="mt-2 text-base font-normal dark:text-neutral-300 text-neutral-500 px-8 text-center">
+              We are working hard to bring you more components. Bookmark this
+              page to stay tuned!
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
