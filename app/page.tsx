0052dec575e8f3ae82282649c9a08049f3e98e16
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-10-10 16:38:29 +0000

    removed unwanted code

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import Hero from "@/components/Hero";
-import AuthThree from "@/components/kaif-ui/blocks/authentication/authOne";
-import AuthTwo from "@/components/kaif-ui/blocks/authentication/authTwo";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <AuthTwo />
-      <AuthThree />
     </>
   );
 }
